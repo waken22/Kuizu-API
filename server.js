253153b6733150a7db1844252cebe0a068fb4d34
Jesé Ramon Barcones <waken22jrb@gmail.com>
2017-11-20 00:09:53 +0100

    new events

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const io = require('socket.io')()
 const PORT = process.env.PORT || 3231
 
 let messages = []
+let users = []
 
 io.on('connection', (socket) => {
+  users.push(socket.id)
+  const newUser = messages.push({ message: socket.id + ' has joined', author: socket.id, connection: true  })
+  io.sockets.emit('new-user', newUser)
+  
   socket.on('new-message', (message) => {
       messages.push(message)
-    io.sockets.emit('chat-message', messages)
+    io.sockets.emit('chat-new-message', message)
     })
+
   socket.on('load-messages', () => {
-    io.sockets.emit('chat-message', messages)
+    io.sockets.emit('chat-messages', messages)
+  })
+
+  socket.on('disconnect', () => {
+    if (socket.id) {
+      delete users[socket.id]
+      messages.push({ message: socket.id + ' has left', author: socket.id, connection: true  })
+      io.sockets.emit('chat-new-message', { message: socket.id + ' has left', author: socket.id, connection: true  })
+    }
   })
 })
+
+
 
 io.listen(PORT)
 console.log('Listening sockets on PORT... : ' + PORT)
