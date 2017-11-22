f4540c451d7bdfd9dfc37971fe95e2b6649e0306
Jesé Ramon Barcones <waken22jrb@gmail.com>
2017-11-22 14:58:27 +0100

    New events added to server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const io = require('socket.io')()
 const PORT = process.env.PORT || 3231
 
 let messages = []
-let users = []
+var users = []
 
 io.on('connection', (socket) => {
   users.push(socket.id)
   const newUser = messages.push({ message: socket.id + ' has joined', author: socket.id, connection: true  })
-  io.sockets.emit('new-user', newUser)
+  io.sockets.emit('get-users', users)
   
   socket.on('new-message', (message) => {
       messages.push(message)
@@ -18,12 +18,15 @@ io.on('connection', (socket) => {
   socket.on('load-messages', () => {
     io.sockets.emit('chat-messages', messages)
   })
-
   socket.on('disconnect', () => {
     if (socket.id) {
-      delete users[socket.id]
+      users = users.filter(function(user) {
+        return user !== socket.id
+      })
+      console.log(users)
       messages.push({ message: socket.id + ' has left', author: socket.id, connection: true  })
       io.sockets.emit('chat-new-message', { message: socket.id + ' has left', author: socket.id, connection: true  })
+      io.sockets.emit('get-users', users)
     }
   })
 })
